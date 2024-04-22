c9feac954caae9e301b79c5d7b6f5265c9776ceb
Gabriel Santos <gabriel.dossantos@usp.br>
2024-04-22 16:37:00 -0300

    Tentando alterar a aba "sobre o site"

--- public/scripts/lerArquivoTXT.js
+++ public/scripts/lerArquivoTXT.js
@@ -1,14 +1,6 @@
-fetch('myText.txt')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error fetching data');
-    }
-    return response.text();
-  })
+fetch('changelog.txt')
+  .then(response => response.text())
   .then(data => {
-    document.getElementById('myText').textContent = data;
+    document.getElementById('output').textContent = data;
   })
-  .catch(error => {
-    console.error(error);
-    document.getElementById('myText').textContent = 'Error fetching data';
-  });
+  .catch(error => console.error(error));
